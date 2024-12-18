6acf6fbf04d6cd5abeb680fb422fca59f3dde198
hackops-gitam <hackopsgitam@gmail.com>
2024-12-18 09:52:47 +0530

    set

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages'; // Proper import of the plugin
+import { ghPages } from 'vite-plugin-gh-pages'; // Proper import of the plugi
 
 export default defineConfig({
   plugins: [react(), ghPages()],
